0f009a1dbc66f3ce8f76b510e7a5da5ee1da3209
comong111 <nyljh0322@gmail.com>
2023-08-31 14:34:59 +0900

    08313131

---
+++ signup.js
@@ -0,0 +1,35 @@
+async function registerUser() {
+    const username = document.getElementById('reg-username').value;
+    const password = document.getElementById('reg-password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+        alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+        return;
+    }
+
+    try {
+        const response = await fetch('/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            alert('회원가입 성공!');
+            window.location.href = '/login';
+        } else {
+            alert('회원가입 실패: ' + data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('회원가입 중 에러가 발생했습니다.');
+    }
+}
