7cf6e39a88484ba87012572c7facc8a7cbc5b413
opixxx <opix0306@naver.com>
2023-09-02 21:08:25 +0900

    RoutineController

---
+++ src/main/java/com/example/project/controller/RoutineController.java
@@ -0,0 +1,21 @@
+package com.example.project.controller;
+
+import com.example.project.Service.RoutineService;
+import com.example.project.domain.Routine;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/routine")
+@RequiredArgsConstructor
+public class RoutineController {
+    private RoutineService routineService;
+
+    @GetMapping("/recommend")
+    public Routine getRecommendedRoutine(@RequestParam int days, @RequestParam String level) {
+        return routineService.getRoutineByCondition(days, level);
+    }
+}
