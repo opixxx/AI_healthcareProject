34f74ee819b617cb4bf3547348983462a7257121
opixxx <opix0306@naver.com>
2023-09-12 23:59:44 +0900

    RoutineRepository @Param 어노테이션 사용

--- src/main/java/com/example/project/repository/RoutineRepository.java
+++ src/main/java/com/example/project/repository/RoutineRepository.java
@@ -5,6 +5,7 @@ import com.example.project.domain.FitnessLevel;
 import com.example.project.domain.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +15,6 @@ public interface RoutineRepository extends JpaRepository<Routine, Long> {
     @Query("select r.routineId" +
             " from Routine r" +
             " where r.dayCount = :days and r.level = :level")
-    List<Long> findRoutineIdByDayCountAndLevel(Days days, FitnessLevel level);
+    List<Long> findRoutineIdByDayCountAndLevel(@Param("days") Days days, @Param("level") FitnessLevel level);
 
 }
